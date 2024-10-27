6834ef06b514a3bddeaf55ada2b55d767a5c17a3
Falling-Bridge <lucmorales2023@inf.udec.cl>
2024-10-27 01:32:15 -0300

    Planteamiento de que hacer a continuación
    -se creo el archivo visualización, para posteriormente crear una interfaz gráfica con las opciones mencionadas
        por el momento es puramente teórico
    
    -Se vá a tratar de romper el código actual

---
+++ include/visualización.h
@@ -0,0 +1,9 @@
+#ifndef VISUALIZACIÓN_H
+#define VISUALIZACIÓN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#endif
---
+++ src/visualización.c
@@ -0,0 +1,9 @@
+#include "../include/visualización.h"
+
+//consultar el tamaño del grafo - Esto es simplemente el valor de n
+//consultar el grafo - Esto es simplemente imprimir el grafo
+//consultar la conexidad del grafo - correr dfs en el grafo original
+//consultar el grado mínimo/máximo del grafo - consultar el campo de cantidad del struct
+//consultar el centro del grafo -consultar el campo de cantidad del grafo
+//consultar los nodos hojas -consultar los nodos que tengan solo un vecino
+//consultar los nodos corte - esto es estúpidamente más complicado
