226be7f6e4f8383696fd7a384874db2fc9467c79
Falling-Bridge <lucmorales2023@inf.udec.cl>
2024-11-15 09:37:02 -0300

    Se cambió la forma de abrir archivos
    En el main se cambió la forma en que buscaba un archivo, en lugar de cambiar la ruta en el código y tener que volver a compilar el mismo;
    se hizo que ahora el usuario ingrese el archivo en el formato especificado

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../include/lector.h"
-#define archivo "./pruebas/grafo1"
 
 int main() {
 
+    char *archivo[100];
+    printf("Ingrese la ruta del archivo, debe seguir el formato: './pruebas/'nombre del archivo'\n\n");
+    scanf("%s", &archivo);
     FILE *lectura = fopen(archivo, "r");
     if (lectura == NULL) {
         perror("Error al abrir el archivo");
